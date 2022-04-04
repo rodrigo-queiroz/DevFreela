eda6e97743617288825563fbbe7e9b9eb4bdff1c
Rodrigo José de Arruda Queiroz <rodrigo.queiroz@kurier.com.br>
2022-04-04 18:11:57 -0300

    Implementando Inversao de dependencia e camada core

---
+++ DevFreela.Core/Entities/UserSkill.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Entities
+{
+    public class UserSkill : BaseEntity
+    {
+        public UserSkill(int idUSer, int idSkill)
+        {
+            IdUSer = idUSer;
+            IdSkill = idSkill;
+        }
+
+        public int IdUSer { get; private set; }
+        public int IdSkill { get; private set; }
+    }
+}
